bf2cb9ab28f21011c7d7568346d730ced80d6ebf
Juan Jose Martinez-Villalba <JJMartinez@wirelessprv-10-192-3-202.near.illinois.edu>
2018-05-07 17:49:06 -0500

    updates to protoype supporting interrupts

--- avalon_mic/avalon_microphone.sv
+++ avalon_mic/avalon_microphone.sv
@@ -13,144 +13,99 @@ module avalon_microphone (
 	input  logic [31:0] AVL_WRITEDATA,	// Avalon-MM Write Data
 	output logic [31:0] AVL_READDATA,	// Avalon-MM Read Data
 	
-	//clock
-	input logic sck,
-	input logic ws,
-	//conduit
+	// Clock signals
+	input logic AUD_BCLK,
+	input logic AUD_ADCLRCK,
+
+	// Data lines from mics
 	input logic GPIO_DIN1,
 	input logic GPIO_DIN2,
-	input logic GPIO_DIN3,
-	input logic GPIO_DIN4,
-	/*
-	input logic GPIO_DIN5,
-	input logic GPIO_DIN6,
-	input logic GPIO_DIN7,
-	input logic GPIO_DIN8,
-	input logic GPIO_DIN9,
-	input logic GPIO_DIN10,
-	*/
-	//export
-	output logic [35:0] lrout
+
+	// Output to codec
+	output logic [31:0] codec_stream,
+	output logic interrupt
 );
 
+logic saw_rise, saw_fall;
 
-logic [35:0] lr_read;
-logic [17:0] mic1_l, mic1_r, mic2_l, mic2_r,  mic3_l, mic3_r, mic4_l, mic4_r;
+assign interrupt = (saw_rise) | (saw_fall);
 
+logic stream_control;
+
+initial begin
+	stream_control = 1;
+end
+
+altera_up_clock_edge detect(
+	.clk(CLK),
+	.reset(reset),
+	.test_clk(ws),
+	.rising_edge(saw_rise),
+	.falling_edge(saw_fall),
+);
+
+logic [31:0] linux_data;
+logic [15:0] mic_l, mic_r;
 //hps to fpga
 logic control_read;
 
 always_comb
 	begin
-	if(control_read)
+	if(stream_control)
 		begin
-		lrout = lr_read;
+		codec_stream = {mic_l, mic_r};
 		end
 	else
 		begin
-		lrout[35:18] = mic1_l;
-		lrout[17:0] = mic1_r;
+		codec_stream = linux_data;
 		end
 	end
 	
 always_ff @ (posedge CLK)
 begin
-
     if(RESET) 
-	 begin
-      lr_read <= 36'b0;
-		control_read <= 0;
-    end 
-
-	 else if (AVL_CS && AVL_WRITE)
+	begin
+		stream_control = 1'b1;
+	end 
+	else if (AVL_CS && AVL_WRITE)
+	begin
+		if (AVL_ADDR == 1'b0)
 		begin
-			if (AVL_ADDR == 4'd0)
-				begin
-				control_read <= AVL_WRITEDATA[0];
-				end
-			else if (AVL_ADDR == 4'd1)
-				begin
-				lr_read[31:0] <= AVL_WRITEDATA;
-				end
-			else if (AVL_ADDR == 4'd2)
-				begin
-				lr_read[35:32] <= AVL_WRITEDATA[3:0];
-				end		
-	
+			stream_control <= AVL_WRITEDATA[0];
 		end
+		else if (AVL_ADDR == 1'b1)
+		begin
+			linux_data = AVL_WRITEDATA;
+		end
+	end
 end
 
 assign codec_control = control_read;
 assign lrout = lr_read;
 
-//wire [15:0] mic_FL, mic_FR, mic_L1, mic_L2, mic_L3, mic_R1, mic_R2, mic_R3, mic_R4, mic_L4;
 
 i2s_master m1(
 	.sck(sck),
 	.ws(ws),
 	.sd(GPIO_DIN1),
-	.data_left(mic1_l),
-	.data_right(mic1_r)
+	.data_left(mic_l),
+	.data_right(mic_r)
 );
-
-i2s_master m2(
-	.sck(sck),
-	.ws(ws),
-	.sd(GPIO_DIN2),
-	.data_left(mic2_l),
-	.data_right(mic2r)
-);
-
-i2s_master m3(
-	.sck(sck),
-	.ws(ws),
-	.sd(GPIO_DIN3),
-	.data_left(mic3_l),
-	.data_right(mic3_r)
-);
-
-i2s_master m4(
-	.sck(sck),
-	.ws(ws),
-	.sd(GPIO_DIN4),
-	.data_left(mic4_l),
-	.data_right(mic4_r)
-);
-
 
 always_comb
 	begin
 		  if (AVL_CS && AVL_READ)
 		  begin
-				if (AVL_ADDR == 4'd0)
+				if (AVL_ADDR == 1'b0)
 					begin
-					AVL_READDATA = {31'b0,ws};
+					AVL_READDATA = {30'd0,saw_rise,saw_fall};
 					end
-				else if (AVL_ADDR == 4'd1)
+				else if (AVL_ADDR == 1'b1)
 					begin
-					AVL_READDATA = {mic1_l[13:0], mic1_r};
+					AVL_READDATA = {mic_l, mic_r};
 					end
-				else if (AVL_ADDR == 4'd2)
-					begin
-					AVL_READDATA = {mic2_l[13:0], mic2_r};
-					end
-				else if (AVL_ADDR == 4'd3)
-					begin
-					AVL_READDATA = {mic3_l[13:0], mic3_r};
-					end
-				else if (AVL_ADDR == 4'd4)
-					begin
-					AVL_READDATA = {mic4_l[13:0], mic4_r};
-					end
-				else if(AVL_ADDR == 4'd5)
-					begin
-					AVL_READDATA = {16'b0, mic1_l[17:14], mic2_l[17:14], mic3_l[17:14], mic4_l[17:14]};
-					end
-				else 
-					AVL_READDATA = 32'b0;
 		  end
 		  else
 				AVL_READDATA = 32'b0;
-
 	end	
 endmodule
